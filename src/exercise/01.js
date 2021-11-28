2d08ec39dd02e91d044cfdfe310dc06ac849e1d1
Ross Mawdsley <rossmawd@gmail.com>
2021-11-28 15:59:44 +0000

    setting an initial value for the 'name' state with a prop

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,9 +3,9 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting({initialName}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const [name, setName] = React.useState()
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
@@ -18,13 +18,18 @@ function Greeting() {
         <label htmlFor="name">Name: </label>
         <input onChange={handleChange} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      {name ? <p>Hello {name}</p> : 'Please type your name'}
     </div>
   )
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Jeff"/>
 }
 
 export default App
